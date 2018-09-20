c571d4d670c802c109d688e750278bd7f648edc7
Samuel Wilson <samuel.wilson@fluxfederation.com>
2018-09-20 13:08:37 +1200

    Initial Commit

---
+++ ExpenseTracker/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ExpenseTracker
+{
+    [Activity(Label = "Expense Tracker", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            EditText amount = FindViewById<EditText>(Resource.Id.Amount);
+            EditText payee = FindViewById<EditText>(Resource.Id.Payee);
+            EditText tags = FindViewById<EditText>(Resource.Id.Tags);
+            Button submit_expense = FindViewById<Button>(Resource.Id.Submit);
+
+            submit_expense.Click += delegate { submit_expense.Text = $"{count++} clicks!"; };
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# expense-tracker
+# Expense Tracker
 Lightweight Budgeting
+
+## Licence
+This repository is published under the MIT licence. As this repository was made during work hack days the copyrights to [Flux Federation](https://fluxfederation.com/).
