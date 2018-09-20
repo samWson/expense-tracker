97f191383423a3243b43298734aeed3aa8a2feba
Samuel Wilson <samuel.wilson@fluxfederation.com>
2018-09-20 14:34:02 +1200

    Create Expenses from GUI input

---
+++ ExpenseTracker/Expense.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ExpenseTracker
+{
+    public class Expense
+    {
+        public float Amount
+        { get; set; }
+
+        public string Payee
+        { get; set; }
+
+        public string Tags
+        { get; set; }
+
+        public DateTime TimeStamp
+        { get; set; }
+
+        public Expense(float amount, string payee, string tags)
+        {
+            this.Amount = amount;
+            this.Payee = payee;
+            this.Tags = tags;
+            this.TimeStamp = DateTime.Now;
+        }
+    }
+}
--- ExpenseTracker/MainActivity.cs
+++ ExpenseTracker/MainActivity.cs
@@ -7,13 +7,10 @@ namespace ExpenseTracker
     [Activity(Label = "Expense Tracker", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
             EditText amount = FindViewById<EditText>(Resource.Id.Amount);
@@ -21,7 +18,11 @@ namespace ExpenseTracker
             EditText tags = FindViewById<EditText>(Resource.Id.Tags);
             Button submit_expense = FindViewById<Button>(Resource.Id.Submit);
 
-            submit_expense.Click += delegate { submit_expense.Text = $"{count++} clicks!"; };
+            submit_expense.Click += delegate 
+            {
+                Expense expense = new Expense(float.Parse(amount.Text), payee.Text, tags.Text);
+                System.Console.WriteLine(expense);
+            };
         }
     }
 }
